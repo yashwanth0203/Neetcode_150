52c2e1aba13d3b5486b48a2b1eae8b0679a2a014
Yashwanth <mvyashwanthkumar@gmail.com>
2025-08-05 00:24:34 +0530

    Used hashtable

---
+++ Downloads/Neetcode_150/contains_duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i])!=mp.end())
+                return true;
+            mp[nums[i]]++;
+    }
+    return false;
+    }
+};
